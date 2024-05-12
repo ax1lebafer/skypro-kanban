798067c64cc6984f453968027afef1e2d3387926
ax1lebafer <raul8917@gmail.com>
2024-05-12 14:35:32 +0400

    Создал тестовый список задач

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const cardList = [
+  {
+    id: 1,
+    topic: "Web Design",
+    title: "Название задачи",
+    date: "12.04.2024",
+    status: "Без статуса",
+  },
+  {
+    id: 2,
+    topic: "Research",
+    title: "Название задачи",
+    date: "12.04.2024",
+    status: "Нужно сделать",
+  },
+  {
+    id: 3,
+    topic: "Copywriting",
+    title: "Название задачи",
+    date: "12.04.2024",
+    status: "В работе",
+  },
+  {
+    id: 4,
+    topic: "Research",
+    title: "Название задачи",
+    date: "12.04.2024",
+    status: "Тестирование",
+  },
+  {
+    id: 5,
+    topic: "Copywriting",
+    title: "Название задачи",
+    date: "12.04.2024",
+    status: "Готово",
+  },
+];
