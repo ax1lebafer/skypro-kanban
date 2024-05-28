ccd4e78becebb3f11519dab15d2187d67cb6ed8d
ax1lebafer <raul8917@gmail.com>
2024-05-28 21:32:48 +0400

    Добавил компонент страницы регистрации

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,54 @@
+import { GlobalStyle } from "../../global.styled";
+import * as S from "./Register.styled";
+
+export const Register = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <S.Wrapper>
+        <S.ContainerSignup>
+          <S.Modal>
+            <S.ModalBlock>
+              <S.ModalTitle>
+                <S.ModalTitleText>Регистрация</S.ModalTitleText>
+              </S.ModalTitle>
+              <S.ModalFormLogin id="formLogUp" action="#">
+                <S.ModalInput
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  placeholder="Имя"
+                />
+                <S.ModalInput
+                  type="text"
+                  name="login"
+                  id="loginReg"
+                  placeholder="Эл. почта"
+                />
+                <S.ModalInput
+                  type="password"
+                  name="password"
+                  id="passwordFirst"
+                  placeholder="Пароль"
+                />
+                <S.ModalButtonSignup id="SignUpEnter">
+                  <S.ModalButtonLink href="../main.html">
+                    Зарегистрироваться
+                  </S.ModalButtonLink>{" "}
+                </S.ModalButtonSignup>
+                <S.ModalFormGroup>
+                  <S.ModalFormText>
+                    Уже есть аккаунт?{" "}
+                    <S.ModalFormLink href="signin.html">
+                      Войдите здесь
+                    </S.ModalFormLink>
+                  </S.ModalFormText>
+                </S.ModalFormGroup>
+              </S.ModalFormLogin>
+            </S.ModalBlock>
+          </S.Modal>
+        </S.ContainerSignup>
+      </S.Wrapper>
+    </>
+  );
+};
