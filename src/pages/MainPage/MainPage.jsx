85cef010739ae80e26fe933742a123b1bdeb1af8
ax1lebafer <raul8917@gmail.com>
2024-06-05 23:35:14 +0400

    Перенес компонент Home в MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,43 +1,13 @@
-import { useEffect, useState } from "react";
-import Main from "../../components/Main/Main";
-import PopNewCard from "../../components/PopNewCard/PopNewCard";
 import { Wrapper } from "../../styles/shared.styled";
-import Header from "../../components/Header/Header";
 import { Outlet } from "react-router-dom";
-import { getTasks } from "../../lib/api";
-import * as S from "./MainPage.styled";
+import Home from "../../components/Home/Home";
 
 const MainPage = ({ userData }) => {
-  const [tasks, setTasks] = useState([]);
-  const [getTasksError, setGetTasksError] = useState(null);
-  const [isLoadingGetTasks, setLoadingGetTasks] = useState(false);
-
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        setLoadingGetTasks(true);
-        const newTasks = await getTasks({token: userData.token});
-        setTasks(newTasks.tasks);
-      } catch (error) {
-        setGetTasksError(error.message);
-      } finally {
-        setLoadingGetTasks(false);
-      }
-    };
-
-    fetchTasks();
-  }, [userData.token]);
 
   return (
     <Wrapper>
-      <PopNewCard />
-
       <Outlet />
-      <Header setTasks={setTasks} tasks={tasks} />
-      {!getTasksError && (
-        <Main taskList={tasks} isLoading={isLoadingGetTasks} />
-      )}
-      {getTasksError && <S.ErrorMessage>{getTasksError}</S.ErrorMessage>}
+      <Home userData={userData}/>
     </Wrapper>
   );
 };
