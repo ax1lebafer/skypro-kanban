8a56fbb16c6252323f37ab4e73cd88fd5a6fed27
ax1lebafer <raul8917@gmail.com>
2024-07-30 22:19:42 +0400

    Подготовил проект к деплою

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import { GlobalStyle } from "./styles/global.styled";
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename="/skypro-kanban/">
       <GlobalStyle />
       <AppRoutes />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import UserProvider from "./contexts/UserContext.jsx";
 import TasksProvider from "./contexts/TasksContext.jsx";
 import ThemeProvider from "./contexts/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <TasksProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </TasksProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <UserProvider>
+      <TasksProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </TasksProvider>
+    </UserProvider>
   </React.StrictMode>
 );
